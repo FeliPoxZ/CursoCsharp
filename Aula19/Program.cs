4e7611039de2a39fd0759e4093777460eae4bee6
Felipe Granvile <felipegranvile71@gmail.com>
2023-11-21 15:37:24 -0300

    aula 19

--- Aula19/Program.cs
+++ Aula19/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula19
+{
+    class Aula19
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            a += 5;
+
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            a /= 2;
+
+            string s = "ABC";
+            Console.WriteLine(s);
+            s += "DEF";
+            Console.WriteLine(s);
+        }
+    }
+}
